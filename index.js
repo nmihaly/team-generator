44a699af2f62a4f67b4e878dee5397e5b5c140e3
Your Name <nickmihaly@gmail.com>
2021-06-27 22:12:19 -0700

    updated lib js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inquirer = require('inquirer');
-
+const fs = require('fs');
 //link lib files
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-var teamMembers = [];
+const teamMembers = [];
 
 const promptUser = () => {
     return inquirer.prompt([
